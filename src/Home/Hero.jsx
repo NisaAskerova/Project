3b2904959d3500a24d49e3e157b74f1bf386b40c
Nisa <nisaaskerova98@gmail.com>
2024-10-25 21:44:20 +0400

    hero

--- src/Home/Hero.jsx
+++ src/Home/Hero.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay, Pagination } from 'swiper/modules';
 
 export default function Hero() {
+  const [slides, setSlides] = useState([]);
+
+  useEffect(() => {
+    const fetchSlides = async () => {
+      const response = await axios.get('http://127.0.0.1:8000/api/sliders');
+      setSlides(response.data);
+    };
+
+    fetchSlides();
+  }, []);
+
   return (
-    <>
-            <div id="homeHero" >
-        <div id='heroLeft'>
-          <h3>Your Safety and
-            Comfort is Our
-            First Priority</h3>
-          <p >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-             The point of using Lorem Ipsum.</p>
-             <button><span className='same'>Get Started</span><img src="./leftIcon.svg" alt="" /></button>
-        </div>
-        <div id="heroRight">
-          <img src="./yellowLine.svg" alt="" />
-          <img src="./portrait-male-security-guard-with-uniform 1.png" alt="" />
-        </div>
-      </div>
-    </>
-  )
+    <Swiper
+      modules={[Autoplay, Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      pagination={{ clickable: true }}
+      loop={true}
+    >
+      {slides.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <div id="homeHero" className={slide.backImage}>
+            <div id='heroLeft'>
+              <h3>{slide.title}</h3>
+              <p>{slide.description}</p>
+              <button>
+                <span className='same'>Get Started</span>
+                <img src={slide.icon} alt="Left Icon" />
+              </button>
+            </div>
+            <div id="heroRight">
+              <img src={slide.heroImage} alt="Line Decoration" />
+              <img src={slide.image} alt="Security Guard" />
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
